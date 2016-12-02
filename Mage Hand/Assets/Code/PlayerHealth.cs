a63e299ca2b3081faa4da8439ad130bcf5d4c4d8
asmccormick <asmccormick@gmail.com>
2016-12-02 14:54:06 +0200

    working on player health

--- Mage Hand/Assets/Code/PlayerHealth.cs
+++ Mage Hand/Assets/Code/PlayerHealth.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SilverAI.Core;
+
 
 public class PlayerHealth : MonoBehaviour {
 
@@ -7,26 +9,60 @@ public class PlayerHealth : MonoBehaviour {
 	private float _currentHealth;
 	[SerializeField] private Renderer _circleOfPainRenderer;
 	[SerializeField] private float _painDecayRate;
+	private SilverAI.Core.Health _healthComponent;
+	[SerializeField] private float _regenerateRate;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		_healthComponent = GetComponent<SilverAI.Core.Health>();
+		_healthComponent.resetHealth(_maxHealth);
+		_healthComponent.resetHealthRegen(_regenerateRate);
+		//InvokeRepeating("checkIfReceivingDamage", 0.1f, 0.1f);
+		InvokeRepeating("checkPlayerHealth", 0.1f, 0.1f);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//checkIfReceivingDamage ();
 		AnimateCircleOfPain();
+		//Debug.Log("current pain = " + _currentPain);
+
 	}
 
 	private void AnimateCircleOfPain ()
 	{
-
-		Debug.Log("pain alpha = " + _circleOfPainRenderer.material.color.a);
-		if (_circleOfPainRenderer.material.color.a > 0)
+		if (_healthComponent.health < 100)
 		{
-			float _tempAlpha = _circleOfPainRenderer.material.color.a - _painDecayRate;
+			_currentPain -= _painDecayRate;
+			float _tempAlpha = _currentPain / 100;
 			Color _tempColor = new Color(_circleOfPainRenderer.material.color.r, _circleOfPainRenderer.material.color.g, _circleOfPainRenderer.material.color.b, _tempAlpha);
 			_circleOfPainRenderer.material.color = _tempColor;
 		}
+	}
+
+	/*
+	void OnCollisionEnter (Collision collision)
+	{
+		Debug.Log("player health collision = " + collision.gameObject.name);
+
+	}
+	*/
+
+	void checkIfReceivingDamage ()
+	{
+		if (_healthComponent.checkIfReceivingDamage() == true)
+		{
+			Debug.Log ("TOOK DAMAGE, damage amount = " + _healthComponent.lastDamage);
+			_currentPain += _healthComponent.lastDamage;
+		}
+	}
+
+	void checkPlayerHealth ()
+	{
+		float _health = _healthComponent.health;
+		Debug.Log("health = " + _health);
+		if (_health < 100)
 	}
 }
